1badcea15a9f6222a36ab2c7d49879ecc17ecd22
Evgeny <LyapinE@gmail.com>
2024-01-17 14:44:58 +0300

    beta v0.1

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import "./index.html";
 import "./style.scss";
 import "@babel/polyfill";
 import "./modules/swiper.js"
+import "./modules/modal.js"
+import "./modules/readMore.js"
+import "./modules/sideMenu.js"
+
